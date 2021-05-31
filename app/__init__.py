f2c4c781d7624511d3c88074f7aca6e7fe14d6ee
Yogesh Bisht <76966100+yogeshBsht@users.noreply.github.com>
2021-05-31 22:30:22 +0530

    Added create_app function
    The function constructs a Flask application instance and registers authentication blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, current_app
+from config import Config
 
-app = Flask(__name__)
-
-from app import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    
+    return app
