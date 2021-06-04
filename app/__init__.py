aa5c32c716e8d13a266a32a9f60f73387240ee93
Yogesh Bisht <76966100+yogeshBsht@users.noreply.github.com>
2021-06-04 22:53:30 +0530

    Update __init__.py
    Created database, migration and login instances. Bound them to the application using init_app function.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask, current_app
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
     
-    return app
+    return app    
+
+from app import models
