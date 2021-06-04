9a02ad684cb7812c20ae6351192843964eabdcd8
Yogesh Bisht <76966100+yogeshBsht@users.noreply.github.com>
2021-06-04 23:03:34 +0530

    Update routes.py
    Updated 'login' route to include user authentication and form validation. Created 'logout' route.

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,13 +1,34 @@
-from flask import render_template
+from flask import render_template, flash, redirect, request
 from app.auth import bp
 from app.auth.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
+
 
 @bp.route('/')
 @bp.route('/index')
+@login_required
 def index():
     return "Hello. This is PG Manager."
     
-@bp.route('/login')
+
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('auth.index'))
     return render_template('auth/login.html', title='Sign In', form=form)
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.index'))
