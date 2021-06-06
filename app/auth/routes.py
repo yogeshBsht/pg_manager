d1b2abcbb6ed56e5bea0c00ca6733d95e56c79a3
yogeshBsht <ybnsit@gmail.com>
2021-06-06 19:25:03 +0530

    Redirect to next page functionality

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -24,6 +24,9 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('auth.index')
         return redirect(url_for('auth.index'))
     return render_template('auth/login.html', title='Sign In', form=form)
 
