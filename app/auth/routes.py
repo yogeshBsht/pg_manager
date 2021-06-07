37484d5af2d169ab41e0a0effb9dc332e3eb6120
yogeshBsht <ybnsit@gmail.com>
2021-06-07 21:52:51 +0530

    Added register route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,16 +1,17 @@
-from flask import render_template, flash, redirect, request
+from flask import url_for, render_template, flash, redirect, request
 from app.auth import bp
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from app import db
 
 
 @bp.route('/')
 @bp.route('/index')
 @login_required
 def index():
-    return "Hello. This is PG Manager."
+    return render_template("index.html", title="Home Page")
     
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -35,3 +36,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('auth.index'))
+
+    
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered tenant!')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', title='Register', form=form)    
